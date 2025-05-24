57b1ed967f1bfcf218ef8924e7e3bf9875440be4
Anup Gupta <anuup22.cs@gmail.com>
2025-05-24 11:31:05 +0000

    chore: import main pages in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Home/Dashboard'
+import HirePrep from './pages/HirePrep/HirePrep'
 
 const App = () => {
   return (
